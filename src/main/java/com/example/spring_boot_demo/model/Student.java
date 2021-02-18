2ff3d609797d17f5c7088acc5230091d6ceeed95
ollleg111 <oleg.maliy14@gmail.com>
2021-02-19 00:09:27 +0200

    add lombok

--- src/main/java/com/example/spring_boot_demo/model/Student.java
+++ src/main/java/com/example/spring_boot_demo/model/Student.java
@@ -1,11 +1,19 @@
 package com.example.spring_boot_demo.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.Period;
 
 @Entity
 @Table
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
+
 public class Student {
 
     @Id
@@ -27,9 +35,6 @@ public class Student {
     @Transient
     private Integer age;
 
-    public Student() {
-    }
-
     public Student(Long id,
                    String name,
                    String email,
@@ -46,56 +51,5 @@ public class Student {
         this.name = name;
         this.email = email;
         this.dob = dob;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public Integer getAge() {
-        return Period.between(this.dob, LocalDate.now()).getYears();
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", dob=" + dob +
-                ", age=" + age +
-                '}';
     }
 }
